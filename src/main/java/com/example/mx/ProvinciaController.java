c06add4920913966fa7bd5485bef8697dcba29f2
Andykingche <laqm_14@hotmail.com>
2020-09-16 12:38:18 -0500

    Se añadio el controller de alumnos y profesores

--- src/main/java/com/example/mx/ProvinciaController.java
+++ src/main/java/com/example/mx/ProvinciaController.java
@@ -1,15 +1,21 @@
 package com.example.mx;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
 import org.apache.tomcat.util.http.HeaderUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
@@ -25,52 +31,63 @@ public class ProvinciaController {
 	@GetMapping("/provincias")
 	public List<Provincia> getAllProvincias() {
 		List<Provincia> page = provinciaRepository.findAll();
+		
+		
 		return page;
 	}
 
 	@GetMapping("/provincias/{id}")
-	public Optional<Provincia> getProvincias(@PathVariable Long id) {
+	public Optional<Provincia> getProvincias(@PathVariable Long id) throws ResourceNotFoundException{
 		Optional<Provincia> provincia = provinciaRepository.findById(id);
+		System.out.println(provincia.get());
 		return provincia;
 	}
  
-	/*
+	
 	@PostMapping("/provincias")
-	public ResponseEntity<Provincia> createProvincias(@Valid @RequestBody Provincia padre) throws URISyntaxException {
+	public ResponseEntity<Provincia> createProvincias(@RequestBody Provincia padre)  {
 		System.out.println(padre.getNombre());
 		System.out.println(padre.getDescripcion());
+		
+		List<Provincia> page = provinciaRepository.findAll();
+		page.size();
+		System.out.println("id "+page.size());
 		if (padre.getId() != null) {
 			return ResponseEntity.badRequest()
-					.headers(
-							HeaderUtil.createFailureAlert("Notas", "idexists", "A new padre cannot already have an ID"))
+					.header("Content-Type", "application/json")
 					.body(null);
 		}
-
+		long idnueva = (page.size() + 1);
+		padre.setId(idnueva);
 		Provincia result = provinciaRepository.save(padre);
-		return ResponseEntity.created(new URI("/api/notas/" + result.getId()))
-				.headers(HeaderUtil.createEntityCreationAlert("Notas", result.getId().toString())).body(result);
+
+		return ResponseEntity.ok()
+				.header("Content-Type", "application/json").body(result);
+		
 	}
 
-	// UPDATE
-	@PutMapping("/provincias")
-	public ResponseEntity<Provincia> updateProvincias(@Valid @RequestBody Provincia provincia)
-			throws URISyntaxException {
-		Provincia result = provinciaRepository.save(provincia);
-		return ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert("NOTA", provincia.getId().toString()))
-				.body(result);
+	@PutMapping("/provincias/{id}")
+	public ResponseEntity<String> updateProvincias( @RequestBody Provincia provincia, @PathVariable Long id) throws ResourceNotFoundException{
+		
+		Provincia encontrarProvincia = getProvincias(id).orElseThrow(( )-> new ResourceNotFoundException("No se encontro el Id"));
+		
+		encontrarProvincia.setNombre(provincia.getNombre());
+		encontrarProvincia.setHabitantes(provincia.getHabitantes());
+		encontrarProvincia.setDescripcion(provincia.getDescripcion());
+		
+		Provincia actualizarProvincia = provinciaRepository.save(encontrarProvincia);		
+		
+		return ResponseEntity.ok()
+				.header("Content-Type", "application/json")
+				.body("{\"mensaje\": \"La provincia Se actualizo correctamente "+""+"\"}");
+		
 	}
+	
 
-	// OK
-	@DeleteMapping("/provincias/{id}")
-	public ResponseEntity<Void> deleteProvincias(@PathVariable Long id) {
-		provinciaRepository.delete(id);
-		return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("provincias", id.toString())).build();
-	}
-	*/
 	@DeleteMapping("/provincias/{id}")
 	public ResponseEntity<String> deleteProvincias(@PathVariable Long id) {
 		provinciaRepository.deleteById(id);
-		//return new ResponseEntity<>("{\"mensaje\": \"Se elimino correctamente el "+id+"\"}",HttpStatus.OK);
+		
 		return ResponseEntity.ok()
 				.header("Content-Type", "application/json")
 				.body("{\"mensaje\": \"Se elimino correctamente el "+id+"\"}");
--- src/main/java/com/example/mx/ProvinciaRepository.java
+++ src/main/java/com/example/mx/ProvinciaRepository.java
@@ -1,7 +1,10 @@
 package com.example.mx;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api")
 public interface ProvinciaRepository extends JpaRepository<Provincia, Long> {
 
 }
