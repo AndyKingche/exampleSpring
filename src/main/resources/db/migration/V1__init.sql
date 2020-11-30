d8234833d9a27e601d697f029b6c49cf012f2e17
Christian <caquespazr@utn.edu.ec>
2020-11-30 18:17:09 -0500

    actualizacion de los datos de migracion provincias-cantones

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `provincia` (
   `id` bigint(20) NOT NULL,
   `nombre` varchar(255) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
    `habitantes` integer
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
