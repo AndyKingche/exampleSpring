80ac777bdc142109c57a70a484d59fa5fb70dff4
Christian <caquespazr@utn.edu.ec>
2020-12-07 21:25:29 -0500

    actualizando los cambios

--- target/classes/db/migration/V9__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `materias` (
-  `id` bigint(20) NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
- `horas` integer,
-
-   `fK_idprofesor` bigint(20),
-   FOREIGN KEY (`fK_idprofesor`) REFERENCES profesor(`id`)
-  
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
