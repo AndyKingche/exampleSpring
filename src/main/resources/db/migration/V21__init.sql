427848acf8ca8ffc17eb455e008dbafcf576d5d4
Andykingche <laqm_14@hotmail.com>
2020-12-15 14:12:57 -0500

    migracion actualizada

--- src/main/resources/db/migration/V21__init.sql
+++ src/main/resources/db/migration/V21__init.sql
@@ -3,8 +3,8 @@ CREATE TABLE `tipo` (
   `nombre` varchar(255) DEFAULT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
   `categoria_id` bigint(20),
-  `estudiante_id` biginit(20),
-  FOREIGN KEY (`estudiante_id`) REFERENCES alumno (`id`),
+  `estudiante_id` bigint(20),
+  FOREIGN KEY (`estudiante_id`) REFERENCES alumnos (`id`),
  FOREIGN KEY (`categoria_id`) REFERENCES categoria (`id`),
  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
